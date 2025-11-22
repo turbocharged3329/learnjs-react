c25956ce38c17f9f563d7ffca7ee4e3ec85e0a10
kitaev_v <kitaev@reezon.ly>
2025-11-22 11:25:01 +0300

    hw-2 - добавлен prettier + отображение списка ресторанов

---
+++ src/main.jsx
@@ -0,0 +1,76 @@
+import { createRoot } from 'react-dom/client'
+import { createElement } from 'react'
+import { restaurants } from './constants/mock.js'
+
+const root = document.getElementById('root')
+const reactRoot = createRoot(root)
+
+reactRoot.render(
+    createElement(
+        'div',
+        { className: 'container' },
+        <>
+            {restaurants.map((restaurant) => {
+                return (
+                    <section
+                        key={restaurant.id}
+                        style={{ borderBottom: '1px solid gray' }}
+                    >
+                        <h2>{restaurant.name}</h2>
+
+                        <div>
+                            <h3>Menu:</h3>
+                            <ul>
+                                {restaurant.menu.map((item) => (
+                                    <li key={item.id}>
+                                        <p
+                                            style={{
+                                                textDecoration: 'underline',
+                                            }}
+                                        >
+                                            {item.name}
+                                        </p>
+
+                                        <b>Price: {item.price}</b>
+
+                                        <div>
+                                            <p>Ingredients:</p>
+                                            <ul>
+                                                {item.ingredients.map(
+                                                    (ingredient) => (
+                                                        <li
+                                                            key={String(
+                                                                ingredient
+                                                            )}
+                                                        >
+                                                            {ingredient}
+                                                        </li>
+                                                    )
+                                                )}
+                                            </ul>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h3>Feedback:</h3>
+                            <ul>
+                                {restaurant.reviews.map((feedbackItem) => (
+                                    <li key={feedbackItem.id}>
+                                        <b>
+                                            {feedbackItem.user} (
+                                            {feedbackItem.rating})
+                                        </b>
+                                        <p>{feedbackItem.text}</p>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </section>
+                )
+            })}
+        </>
+    )
+)
