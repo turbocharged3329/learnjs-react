7ebbf81cdd0a92701eea439d4065176ae0c3f7cc
Kitaev Vlad <v.kitaev@napoleonit.ru>
2025-12-18 22:04:27 +0300

    hw-6 - рефакторинг

--- src/components/layouts/default/theme-switcher.jsx
+++ src/components/layouts/default/theme-switcher.jsx
@@ -4,14 +4,12 @@ import { ThemeContext } from '@/components/theme-context/theme-context.jsx'
 import { COLOR_THEME_VALUES } from '@/constants/index.js'
 
 export const ThemeSwitcher = ({ className }) => {
-    const { theme, setTheme } = useContext(ThemeContext)
+    const { theme, setThemeHandler } = useContext(ThemeContext)
     const isLight = useMemo(() => theme === COLOR_THEME_VALUES.light, [theme])
 
     function handleThemeToggle() {
-        setTheme((current) =>
-            current === COLOR_THEME_VALUES.light
-                ? COLOR_THEME_VALUES.black
-                : COLOR_THEME_VALUES.light
+        setThemeHandler(
+            isLight ? COLOR_THEME_VALUES.black : COLOR_THEME_VALUES.light
         )
     }
 
--- src/components/theme-context/theme-context-provider.jsx
+++ src/components/theme-context/theme-context-provider.jsx
@@ -1,16 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import { ThemeContext } from '@/components/theme-context/theme-context.jsx'
 import { COLOR_THEME_VALUES } from '@/constants/index.js'
 
 export const ThemeContextProvider = ({ children }) => {
-    const [theme, setTheme] = useState(COLOR_THEME_VALUES.black)
+    const [theme, setTheme] = useState(COLOR_THEME_VALUES.light)
+
+    const setDocumentDatasetTheme = (themeValue) => {
+        document.documentElement.dataset.theme = themeValue
+    }
 
     useEffect(() => {
-        document.documentElement.dataset.theme = theme
-    }, [theme])
+        setDocumentDatasetTheme(theme)
+    })
+
+    const setThemeHandler = (themeValue) => {
+        setTheme(themeValue)
+        setDocumentDatasetTheme(themeValue)
+    }
 
     return (
-        <ThemeContext.Provider value={{ theme, setTheme }}>
+        <ThemeContext.Provider value={{ theme, setThemeHandler }}>
             {children}
         </ThemeContext.Provider>
     )
